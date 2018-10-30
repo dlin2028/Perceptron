dbaae4eca049dc2352571dfdf7fecaffdb015451
dlin2028 <dlin2028@gmail.com>
2018-10-29 22:03:54 -0700

    initial

---
+++ Perceptron/Perceptron.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Perceptron
+{
+    class Perceptron
+    {
+        public double[] Weights;
+        double BiasWeight;
+
+        public Perceptron(int numberOfInputs)
+        {
+            Weights = new double[numberOfInputs];
+        }
+
+        public double Compute(double[] inputs)
+        {
+            if (inputs.Length != Weights.Length)
+            {
+                throw new Exception("wrong number of inputs");
+            }
+
+            double dotProduct = inputs.Zip(Weights, (d1, d2) => d1 * d2).Sum();
+            if(dotProduct + BiasWeight > 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+  
+    }
+}
---
+++ Perceptron/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Perceptron
+{
+    class Program
+    {
+        struct Datum
+        {
+            public double[] input;
+            public double ouput;
+
+            public Datum(double[] input, double output)
+            {
+                this.input = input;
+                this.ouput = output;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Perceptron perceptron = new Perceptron(2);
+
+            List<Datum> data = new List<Datum>();
+            data.Add(new Datum(new double[] { 1, 1 }, 1));
+            data.Add(new Datum(new double[] { 1, 0 }, 0));
+            data.Add(new Datum(new double[] { 0, 1 }, 0));
+            data.Add(new Datum(new double[] { 0, 0 }, 0));
+            
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                double output = perceptron.Compute(data[i].input);
+                Console.WriteLine(output);
+                if (output != data[i].ouput)
+                {
+                    for (int j = 0; j < 1; j++)
+                    {
+                        perceptron.Weights[j] = perceptron.Weights[j] + (data[i].ouput - output) * data[i].input[j];
+                    }
+                }
+            }
+
+            while(true)
+            {
+                Console.WriteLine(perceptron.Compute(
+                    new double[] {
+                        double.Parse(Console.ReadLine()),
+                        double.Parse(Console.ReadLine())
+                    }));
+            }
+
+        }
+    }
+}
